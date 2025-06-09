a184b2a7bde8dab91a43db65e1e0cf55b051e022
Ture Sayer <ture.sayer@posteo.de>
2025-06-09 15:16:40 +0200

    Add a simple serving of frontend

--- backend/src/main/kotlin/Routing.kt
+++ backend/src/main/kotlin/Routing.kt
@@ -1,13 +1,18 @@
 package koeln.sayer
 
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+//        get("/") {
+//            call.respondText("Hello World!")
+//        }
+        singlePageApplication {
+            useResources = true
+            angular("frontend/browser")
         }
     }
 }
