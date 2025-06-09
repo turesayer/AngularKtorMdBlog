1e7d813be3aa9922fda3d7d2db94d20d38261cfc
Ture Sayer <ture.sayer@posteo.de>
2025-06-09 15:08:32 +0200

    Init backend subproject

---
+++ backend/build.gradle.kts
@@ -0,0 +1,30 @@
+val kotlin_version: String by project
+val ktor_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.1.21"
+    id("io.ktor.plugin") version "3.1.3"
+}
+
+group = "koeln.sayer"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
