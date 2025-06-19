e6615a56429a5228ced36fbc33687618a8f59b06
Ture Sayer <ture.sayer@posteo.de>
2025-06-20 00:05:47 +0200

    added an angular dev env so that loading list of post from backend works in final build

--- frontend/src/app/services/frontendDataApi/frontend-data-api.service.ts
+++ frontend/src/app/services/frontendDataApi/frontend-data-api.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {inject, Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 export interface BlogPost {
   title: string;
@@ -12,10 +13,11 @@ export interface BlogPost {
 })
 export class FrontendDataApiService {
   private http = inject(HttpClient)
+  private apiBaseUrl = environment.apiBaseUrl;
 
   constructor() { }
 
   getBlogPosts(): Observable<BlogPost[]> {
-    return this.http.get<BlogPost[]>('http://localhost:8080/api/frontenddata/posts');
+    return this.http.get<BlogPost[]>(`${this.apiBaseUrl}/frontenddata/posts`);
   }
 }
